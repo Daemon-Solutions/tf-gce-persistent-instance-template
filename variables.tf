03b7b8e68030cf40cae18abf578391d29008df07
Marcin Kulisz <marcin@bashton.com>
2016-11-23 10:19:39 +0000

    Switching scope to native lists and running hclfmt

--- variables.tf
+++ variables.tf
@@ -1,22 +1,41 @@
 # Instance template variables
-variable "envname" { }
-variable "service" { }
-variable "domain" { }
-variable "fw_tags" { type = "list" }
-variable "machine_type" { default = "g1-small" }
-variable "disk_device_name" { default = "sda" }
-variable "disk_image" { }
-variable "net_name" { }
-variable "scopes" { default = "storage-ro,compute-ro" }
-variable "persistent_disk" { }
-variable "persistent_disk_name" { default = "sdc" }
-variable "startup_script" { }
-variable "shortzone" { }
-
+variable "envname" {}
+variable "service" {}
+variable "domain" {}
+variable "disk_image" {}
+variable "net_name" {}
+variable "persistent_disk" {}
+variable "startup_script" {}
+variable "shortzone" {}
 variable gcp_region {}
 variable needs_nat {}
 variable subnets_enabled {}
 variable network_enabled {}
 
+variable "fw_tags" {
+  type = "list"
+}
+
+variable "machine_type" {
+  default = "g1-small"
+}
+
+variable "disk_device_name" {
+  default = "sda"
+}
+
+variable "scopes" {
+  default = [
+    "storage-ro",
+    "compute-ro",
+  ]
+}
+
+variable "persistent_disk_name" {
+  default = "sdc"
+}
+
 # Needed for NAT
-variable "ip_forward" { default = "true" }
+variable "ip_forward" {
+  default = "true"
+}
